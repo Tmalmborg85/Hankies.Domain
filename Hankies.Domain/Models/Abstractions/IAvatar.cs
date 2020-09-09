49302a75c48cb0d190b958e0a248ba35e7039056
Tmalmborg85 <thomas.e.malmborg@gmail.com>
2020-09-09 11:49:10 -0700

    non working commit. changed IAvatar

--- Hankies.Domain/Models/Abstractions/IAvatar.cs
+++ Hankies.Domain/Models/Abstractions/IAvatar.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Hankies.Domain.Models.Details;
 
 namespace Hankies.Domain.Models.Abstractions
 {
@@ -20,6 +21,7 @@ namespace Hankies.Domain.Models.Abstractions
         public ICustomer Owner { get; }
         public CruiseStatuses Status { get; }
         public IPhoto ImpressionPhoto { get; }
+
         public bool Blindfolded { get; }
         public bool Hooded { get; }
         public ICruiseCoordinates LastKnownLocation { get; }
@@ -30,14 +32,41 @@ namespace Hankies.Domain.Models.Abstractions
         /// <summary>
         /// Other avatars this avatar has cruised.
         /// </summary>
-        IEnumerable<IAvatar> Cruised { get; }
+        IEnumerable<IAvatar> CruisedAvatars { get; }
 
         /// <summary>
         /// Other avatars that have cruised this avatar
         /// </summary>
-        IEnumerable<IAvatar> CruisedBy { get; }
+        IEnumerable<IAvatar> CruisedByAvatars { get; }
 
-        IEnumerable<IAvatar> ClosestAvatars
+        /// <summary>
+        /// Avatars that match the seeking gener, handkerchief rolls, and area
+        /// </summary>
+        IEnumerable<IAvatar> Matches { get; }
+
+        /// <summary>
+        /// Avatars that match two of three of these. Seeking gener,
+        /// handkerchief rolls, and area
+        /// </summary>
+        IEnumerable<IAvatar> NearMatches { get; }
+
+
+        void CruiseAvatar(IAvatar cruisee);
+        void CruisedBy(IAvatar cruisee);
+
+        IStatus<IAvatar> CruiseForAvatars();
+        IStatus<IAvatar> PauseCruising();
+        IStatus<IAvatar> EndCruising();
+
+        IStatus<IAvatar> PutOnBlindfold();
+        IStatus<IAvatar> TakeOffBlindfold();
+        IStatus<IAvatar> PutOnHood();
+        IStatus<IAvatar> TakeOffHood();
+
+        public IEnumerable<IAvatar> WearHandkerchiefInPocket(IHandkerchief
+            handkerchief, PocketTypes pocket);
+        public IEnumerable<IAvatar> RemoveHandkerchiefFromPocket(IHandkerchief
+            handkerchief, PocketTypes pocket);
     }
 }
     
