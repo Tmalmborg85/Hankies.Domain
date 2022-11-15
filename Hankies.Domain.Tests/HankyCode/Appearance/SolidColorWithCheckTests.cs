f1f3142cea2607b7be72bf64c7be1cfd84cd1eb2
TomOfHankies <thomas.e.malmborg@gmail.com>
2022-11-15 11:17:57 -0600

    Added solid color with check
    so far the code only uses black with white check but it makes sence to leave room for more.

---
+++ Hankies.Domain.Tests/HankyCode/Appearance/SolidColorWithCheckTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Hankies.Domain.HankyCode.Appearance;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hankies.Domain.Tests.HankyCode.Appearance
+{
+    [TestClass]
+    public class SolidColorWithCheckTests
+    {
+        [TestMethod]
+        public void SolidColorWCheckDescriptionIsCorrect()
+        {
+            //Arrange
+            var blackColor = new NamedColor("Black", "#000000");
+            var whiteColor = new NamedColor("White", "#ffffff");
+            var expectedDescription = "Black with white check";
+
+            var blackWithWhiteCheck = new SolidColorWithCheck(blackColor, whiteColor);
+
+            //Assert
+            Assert.AreEqual(expectedDescription, blackWithWhiteCheck.Description);
+
+        }
+
+        [TestMethod]
+        public void SolidColorWCheckIDIsCorrect()
+        {
+            //Arrange
+            var blackColor = new NamedColor("Black", "#000000");
+            var whiteColor = new NamedColor("White", "#ffffff");
+            var expectedID = "BLACKWITHWHITECHECK";
+
+            var blackWithWhiteCheck = new SolidColorWithCheck(blackColor, whiteColor);
+
+            //Assert
+            Assert.AreEqual(expectedID, blackWithWhiteCheck.ID);
+
+        }
+    }
+}
---
+++ Hankies.Domain/HankyCode/Appearance/SolidColorWithCheck.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Hankies.Domain.HankyCode.Appearance
+{
+    public class SolidColorWithCheck : Appearance, ISolidColor
+    {
+        public NamedColor Color { get; private set; }
+        public NamedColor CheckColor { get; private set; }
+
+        public override string Description => Color.Name + " with " + CheckColor.Name.ToLower() + " check";
+
+        public SolidColorWithCheck(NamedColor solidColor, NamedColor checkColor)
+        {
+            Color = solidColor;
+            CheckColor = checkColor;
+        }
+    }
+}
