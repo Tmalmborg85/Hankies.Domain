89b42cdd8d59204e2c557dc1e7daef9a972dbc02
TomOfHankies <thomas.e.malmborg@gmail.com>
2022-11-15 10:48:40 -0600

    Added SolidColorWDots

---
+++ Hankies.Domain.Tests/HankyCode/Appearance/SolidColorWithDotsTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Hankies.Domain.HankyCode.Appearance;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hankies.Domain.Tests.HankyCode.Appearance
+{
+    [TestClass]
+    public class SolidColorWithDotsTests
+    {
+        [TestMethod]
+        public void SolidColorWDotsDescriptionIsCorrect()
+        {
+            //Arrange
+            var blueColor = new NamedColor("Blue", "#0000CD");
+            var whiteColor = new NamedColor("White", "#ffffff");
+            var expectedDescription = "Blue with white dots";
+
+            var blueWithWhiteDots = new SolidColorWithDots(blueColor, whiteColor);
+
+            //Assert
+            Assert.AreEqual(expectedDescription, blueWithWhiteDots.Description);
+
+        }
+    }
+}
---
+++ Hankies.Domain/HankyCode/Appearance/SolidColorWithDots.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Hankies.Domain.HankyCode.Appearance
+{
+    public class SolidColorWithDots : Appearance, ISolidColor
+    {
+        public NamedColor Color { get; private set; }
+        public NamedColor DotColor { get; private set; }
+
+        public override string Description => Color.Name + " with " + DotColor.Name.ToLower() + " dots";
+
+        public SolidColorWithDots(NamedColor solidColor, NamedColor dotColor)
+        {
+            Color = solidColor;
+            DotColor = dotColor;
+        }
+    }
+}
