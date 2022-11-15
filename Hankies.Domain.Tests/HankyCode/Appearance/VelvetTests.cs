b1ba626352d876a44788d272df59e5d5140429b9
TomOfHankies <thomas.e.malmborg@gmail.com>
2022-11-15 11:32:18 -0600

    Added Velvet

---
+++ Hankies.Domain.Tests/HankyCode/Appearance/VelvetTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Hankies.Domain.HankyCode.Appearance;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hankies.Domain.Tests.HankyCode.Appearance
+{
+    [TestClass]
+    public class VelvetTests
+    {
+        [TestMethod]
+        public void VelvetDescriptionIsCorrect()
+        {
+            //Arrange
+            var whiteColor = new NamedColor("White", "#ffffff");
+            var expectedDescription = "White velvet";
+
+            var whiteVelvet = new Velvet(whiteColor);
+
+            //Assert
+            Assert.AreEqual(expectedDescription, whiteVelvet.Description);
+
+        }
+
+        [TestMethod]
+        public void VelvetIDIsCorrect()
+        {
+            //Arrange
+            var whiteColor = new NamedColor("White", "#ffffff");
+            var expectedID = "WHITEVELVET";
+
+            var whiteVelvet = new Velvet(whiteColor);
+
+            //Assert
+            Assert.AreEqual(expectedID, whiteVelvet.ID);
+        }
+    }
+}
--- Hankies.Domain/HankyCode/Appearance/Velvet.cs
+++ Hankies.Domain/HankyCode/Appearance/Velvet.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 namespace Hankies.Domain.HankyCode.Appearance
 {
-    public class Velvet
+    public class Velvet : Appearance, ISolidColor
     {
-        public Velvet()
+        public NamedColor Color { get; private set; }
+
+        public override string Description => Color.Name + " velvet";
+
+        public Velvet(NamedColor solidColor)
         {
+            Color = solidColor;
         }
     }
 }
